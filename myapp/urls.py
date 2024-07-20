0578cda1d8da648413dfe0914925058546a2045e
Alka Wilson <alkarhine@gmail.com>
2024-07-20 15:29:27 -0400

    update functionality

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from  . import views
 
-urlpatterns = [path('create_booking/', views.create_booking),
-               path('success/',views.booking_success,name='booking_success'),
+urlpatterns = [path('', views.create_booking),
+               path('success/<int:pk>/',views.booking_success,name='booking_success'),
                path('list/',views.booking_list,name='booking_list'),
-                 path('details/<int:pk>/',views.booking_details,name='booking_details'),
+               path('details/<int:pk>/',views.booking_details,name='booking_details'),
+               path('details/<int:pk>/edit',views.booking_update,name='booking_update'),
+
 
                ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from .forms import BookingForm
-from .models import Booking, Category
+from .models import Booking
 
 # Create your views here.
 from django.http import HttpResponse
@@ -11,15 +11,16 @@ def create_booking(request):
     if request.method == 'POST':
         form = BookingForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('booking_success')
+            booking = form.save()
+            return redirect('booking_success',pk=booking.pk)
     else:
         form = BookingForm()
-    context = {"form" : form}
-    return render(request, 'create_booking.html', context)
 
-def booking_success(request):
-    return HttpResponse("Booking created successfully!")
+    return render(request, 'create_booking.html',{'form':form})
+
+def booking_success(request,pk):
+    booking = get_object_or_404(Booking,pk=pk)
+    return render(request,'booking_success.html',{'booking':booking})
 
 #Read
 def booking_list(request):
@@ -29,3 +30,16 @@ def booking_list(request):
 def booking_details(request,pk):
     booking = get_object_or_404(Booking,pk=pk)
     return render(request,'booking_details.html',{'booking':booking})
+
+#update
+def booking_update(request,pk):
+    booking =get_object_or_404(Booking,pk=pk)
+    if request.method == 'POST':
+        form = BookingForm(request.POST,instance=booking)
+        if form.is_valid():
+            form.save()
+            return redirect('booking_details',pk=pk)
+
+    else:
+        form = BookingForm(instance=booking)
+    return render(request,'booking_update.html',{'form':form})
